6c475c8d7ca36fd95ccf4f2a70ea4757c657a05e
shoshin-programmer <atienza.new@gmail.com>
2020-12-03 00:21:00 +0800

    updated projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,12 +13,9 @@ const Projects: React.FunctionComponent<{}> = (): React.ReactElement => {
         <title>The Library</title>
       </Head>
       <Header />
-      <div
-        id="project-section"
-        className="hero parallax-img full-height"
-      >
+      <div id="project-section" className="hero parallax-img full-height">
         <div className="hero-body">
-          <div className="content u-text-left p-3">
+          <div className="content u-text-left p-3 content-sm">
             <h1 className="headline-5 mt-5 text-gray-400">THE LIBRARY</h1>
             {project_list.map((item, idx) => (
               <Fade key={idx} delay={startfade + idx * 300} bottom>
@@ -39,23 +36,41 @@ export default Projects;
 function Tile({ item }) {
   return (
     <div className="row">
-      <div className="col-md-12 bg-black mt-5">
+      <div className="col-md-12 bg-black mt-2">
         <div className="tile white p-2 library-tiles">
-          <div className="tile__container">
+          <div className="tile__container u-text-ellipsis">
             <p className="tile__title m-0 u-text-ellipsis">{item.title}</p>
-            <p className="tile__subtitle m-0">{item.subtitle}</p>
+            <p className="tile__subtitle m-0 u-text-ellipsis">
+              {item.subtitle}
+            </p>
             <span className="info">{item.year}</span>
           </div>
-          <div className="tile__buttons p-2">
-            <div className="btn-group">
-              {item.links.map((link, idx) => (
-                <Link href={link.url} key={idx}>
-                  <a target="_blank" className="btn btn-dark btn-small uppercase">
-                    {link.name}
-                  </a>
-                </Link>
-              ))}
-            </div>
+          <div className="tile__buttons">
+            {item.links.map((link, idx) => (
+              <Link href={link.url} key={idx}>
+                <a
+                  target="_blank"
+                  className="btn btn-dark btn-xsmall uppercase"
+                >
+                  {link.name == "Github" ? (
+                    <span className="icon">
+                      <i
+                        className="fab fa-wrapper fa-github"
+                        aria-hidden="true"
+                      ></i>
+                    </span>
+                  ) : (
+                    <span className="icon">
+                      <i
+                        className="fas fa-wrapper fa-external-link-alt"
+                        aria-hidden="true"
+                      ></i>
+                    </span>
+
+                  )}
+                </a>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
