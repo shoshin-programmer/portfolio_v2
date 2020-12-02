a170ad3e2fa326a4387412ba31d3fa6e2d3bc8b5
shoshin-programmer <atienza.new@gmail.com>
2020-12-03 00:28:45 +0800

    updated project list

--- data/project_list.js
+++ data/project_list.js
@@ -31,6 +31,42 @@ const project_list = [
       },
     ],
   },
+  {
+    title: "NextJS with MUI Template",
+    subtitle:
+      "NextJS doesn't immediately work with MUI since NextJS is an SSR framework, I made a template for ease of use.",
+    year: "2020",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/shoshin-programmer/next_with_mui_template",
+      },
+    ],
+  },
+  {
+    title: "Excel Generator ( Pseudocode )",
+    subtitle:
+      "Small project but proprietary to my former company using pandas to generate excel reports based on data.",
+    year: "2019",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/shoshin-programmer/excel_generator_pseudocode",
+      },
+    ],
+  },
+  {
+    title: "React with Django Boilerplate",
+    subtitle:
+      "Boilerplate for using React for the frontend with only Django as the server.",
+    year: "2019",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/shoshin-programmer/react_django_boilerplate",
+      },
+    ],
+  },
 ];
 
 export default project_list;
