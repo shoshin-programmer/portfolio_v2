358d42683af88d91e22e4186a95af781811e2f24
shoshin-programmer <atienza.new@gmail.com>
2020-12-17 16:02:52 +0800

    re update og to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,58 +1,9 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import Head from "next/head";
-import "../styles/globals.css"
+import "../styles/globals.css";
 import "cirrus-ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-        />
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge;" />
-        <meta
-          prefix="og: http://ogp.me/ns#"
-          property="og:type"
-          content="Portfolio for Mark the Dev."
-        />
-
-        <meta
-          prefix="og: http://ogp.me/ns#"
-          property="og:title"
-          content="Mark The Dev - The Portfolio"
-        />
-
-        <meta
-          prefix="og: http://ogp.me/ns#"
-          property="og:description"
-          content="Portfolio for sample projects and more details."
-        />
-
-        <meta
-          prefix="og: http://ogp.me/ns#"
-          property="og:image"
-          content="/web.png"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Montserrat:400,700"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
-          integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
-          crossOrigin="anonymous"
-        ></link>
-      </Head>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,22 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Mark The Dev</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+          />
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge;" />
+          <meta property="og:type" content="Portfolio for Mark the Dev." />
+          <meta property="og:title" content="Mark The Dev - The Portfolio" />
+          <meta property="og:url" content="https://www.mark-atienza.com/" />
+          <meta property="og:image" content="/web.png" />
+          <meta
+            property="og:description"
+            content="Portfolio for sample projects and more details."
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
@@ -24,6 +40,21 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+          <link
+            href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:400,700"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            crossOrigin="anonymous"
+          ></link>
         </Head>
         <body>
           <Main />
